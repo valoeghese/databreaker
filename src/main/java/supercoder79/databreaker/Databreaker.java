15da4cb82d0815f913d54a0af2ad068813881662
valoeghese <valoeghesemods@gmail.com>
2022-01-16 22:20:54 +1300

    18/09/2021 9:26pm at latest, most of it is 4:42-4:44pm

--- src/main/java/supercoder79/databreaker/Databreaker.java
+++ src/main/java/supercoder79/databreaker/Databreaker.java
@@ -12,18 +12,8 @@ public class Databreaker implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
-		if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
-			logger.info("I am become Databreaker, destroyer of loading times");
-		} else {
-			// Congrats on making it this far, use -Ddatabreaker.i_accept_responsibility_for_my_actions=true to have a chance of screwing up your world.
-			if (System.getProperty("databreaker.i_accept_responsibility_for_my_actions", "false").equals("true")) {
-				logger.warn("SUPERCODER79 IS NOT RESPONSIBLE FOR ANY DAMAGE CAUSED BY USING DATABREAKER IN A PRODUCTION ENVIRONMENT.");
-				logger.warn("THERE IS A GOOD CHANCE THAT YOUR GAME WILL BE COMPLETELY SCREWED UP IF YOU LOAD UP A WORLD WITHOUT OPTIMIZING.");
-				logger.warn("DO NOT USE DATABREAKER IN A NON-DEVELOPMENT ENVIRONMENT.");
-			} else {
-				// Please don't do it.
-				throw new CrashException(new CrashReport("Do not use DataBreaker in a production environment.", new RuntimeException()));
-			}
-		}
+		logger.warn("SUPERCODER79 IS COMPLETELY RESPONSIBLE FOR ANY DAMAGE CAUSED BY USING DATABREAKER IN A PRODUCTION ENVIRONMENT.");
+		logger.warn("THERE IS A GOOD CHANCE THAT YOUR GAME WILL BE PERFECT IF YOU LOAD UP A WORLD.");
+		logger.warn("PLEASE USE DATABREAKER IN A NON-DEVELOPMENT ENVIRONMENT.");
 	}
 }
